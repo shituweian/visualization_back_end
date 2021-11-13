9c2bfdeaf9d4a763b760c552a18dc322f65b346d
Tian Yu <17722024@bjtu.edu.cn>
2021-11-13 16:52:22 +0800

    v1.6 Daily API

---
+++ src/main/java/comp7507/api_server/Entity/CalendarPieElement.java
@@ -0,0 +1,23 @@
+package comp7507.api_server.Entity;
+
+public class CalendarPieElement
+{
+    private String name;
+    private int value;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
